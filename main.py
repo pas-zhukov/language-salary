5581f1ed192253fb541310b5842a876d34e4bf18
Pavel <pas-zhukov@yandex.ru>
2023-06-28 23:28:19 +0300

    continue developing

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+token = "v3.r.137457653.7ea316e15b259c72cbb425cd3ad5a3eef3eb21a2.fbe8484fa854ad9fd2cc55f3776bd60959941488"
+
+header = {"X-Api-App-Id": token}
+url = "https://api.superjob.ru/2.0/vacancies/"
+
+params = {
+
+}
+
+response = requests.get(url, headers=header)
+for obj in response.json()['objects']:
+    print(obj['profession'])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
