39380896d065470130cc7c486b11c8660e354497
Pavel <pas-zhukov@yandex.ru>
2023-06-29 16:55:54 +0300

    dry code, place it to main

--- hh.py
+++ hh.py
@@ -38,9 +38,7 @@ def predict_salary(salary_from, salary_to,
     elif salary_to:
         salary = salary_to_coef * salary_to
     else:
-        print(None)
         return None
-    print(salary)
     return salary
 
 
@@ -53,93 +51,92 @@ def predict_rub_salary_hh(vacancy: dict):
 
 
 def predict_rub_salary_superjob(vacancy: dict):
-    print(vacancy['payment_from'],
-          vacancy['payment_to'], sep='-')
     if vacancy['currency'] == 'rub':
         return predict_salary(vacancy['payment_from'],
                               vacancy['payment_to'])
     return None
 
+
 def get_vacancies_statistics(
+        api_method_url: str,
         max_vacancies_quantity: int,
         vacancies_per_page: int,
         text_field_name: str,
         location_field_name: str,
+        items_field_name: str,
+        city_id: int,
+        salary_prediction_function: callable,
         period_field_name: str = 'period',
         page_number_field_name: str = 'page',
         count_per_page_field_name: str = 'per_page',
         request_headers: dict = None,
         additional_request_params: dict = None,
+        requests_delay: int = 5,
+        period: int = 30
         ) -> dict:
-    pass
+    vacancies_by_language = {key: {} for key in POPULAR_LANGUAGES}
+    for language in tqdm(POPULAR_LANGUAGES):
+        total_salaries = []
+        vacancies_found = 0
+        vacancies_processed = 0
+        for page_number in range(int(max_vacancies_quantity / vacancies_per_page)):
+            headers = request_headers
+            params = {
+                text_field_name: f'Программист {language}',
+                location_field_name: city_id,
+                period_field_name: period,
+                page_number_field_name: page_number,
+                count_per_page_field_name: vacancies_per_page,
+            }
+            if additional_request_params:
+                params += additional_request_params
+            response = requests.get(api_method_url, headers=headers, params=params)
+            response.raise_for_status()
+            vacancies = response.json()[items_field_name]
+            vacancies_found += len(vacancies)
+            salaries = []
+            for vacancy in vacancies:
+                salary = salary_prediction_function(vacancy)
+                if salary:
+                    salaries.append(salary)
+                    vacancies_processed += 1
+            total_salaries.append(salaries)
+            time.sleep(1)
+        vacancies_by_language[language]['average_salary'] = int(np.mean([x for y in total_salaries for x in y]))
+        vacancies_by_language[language]['vacancies_found'] = vacancies_found
+        vacancies_by_language[language]['vacancies_processed'] = vacancies_processed
+        time.sleep(requests_delay)
+    return vacancies_by_language
 
 
 def get_vacancies_statistics_hh(period: int = 30):
-    vacancies_by_language = {key: {} for key in POPULAR_LANGUAGES}
-    for language in tqdm(POPULAR_LANGUAGES):
-        total_salaries = []
-        vacancies_found = 0
-        vacancies_processed = 0
-        for page_number in range(int(SJ_MAX_VACANCIES_QUANTITY / SJ_VACANCIES_PER_PAGE)):
-            params = {
-                'text': f'Программист {language}',
-                'area': HH_MOSCOW_ID,
-                'period': period,
-                'page': page_number,
-                'per_page': HH_VACANCIES_PER_PAGE,
-                'only_with_salary': True
-            }
-            response = requests.get(HH_API_METHOD_URL, params)
-            response.raise_for_status()
-            vacancies = response.json()['items']
-            vacancies_found += len(vacancies)
-            salaries = []
-            for vacancy in vacancies:
-                salary = predict_rub_salary_hh(vacancy)
-                if salary:
-                    salaries.append(salary)
-                    vacancies_processed += 1
-            total_salaries.append(salaries)
-            time.sleep(1)
-        vacancies_by_language[language]['average_salary'] = int(np.mean([x for y in total_salaries for x in y]))
-        vacancies_by_language[language]['vacancies_found'] = vacancies_found
-        vacancies_by_language[language]['vacancies_processed'] = vacancies_processed
-        time.sleep(5)
-    return vacancies_by_language
+    additional_request_param = {'only_with_salary': True}
+
+    statistics = get_vacancies_statistics(HH_API_METHOD_URL, HH_MAX_VACANCIES_QUANTITY, HH_VACANCIES_PER_PAGE,
+                                          text_field_name='text',
+                                          location_field_name='area',
+                                          items_field_name='items',
+                                          salary_prediction_function=predict_rub_salary_hh,
+                                          city_id=HH_MOSCOW_ID,
+                                          period=period,
+                                          additional_request_params=additional_request_param,
+                                          requests_delay=10)
+    return statistics
 
 
 def get_vacancies_statistics_sj(sj_secret_key: str, period: int = 30):
-    vacancies_by_language = {key: {} for key in POPULAR_LANGUAGES}
-    for language in tqdm(POPULAR_LANGUAGES):
-        total_salaries = []
-        vacancies_found = 0
-        vacancies_processed = 0
-        for page_number in range(int(SJ_MAX_VACANCIES_QUANTITY / SJ_VACANCIES_PER_PAGE)):
-            headers = {"X-Api-App-Id": sj_secret_key}
-            params = {
-                'keyword': f'Программист {language}',
-                'town': SJ_MOSCOW_ID,
-                'period': period,
-                'page': page_number,
-                'count': SJ_VACANCIES_PER_PAGE,
-            }
-            response = requests.get(SJ_API_METHOD_URL, headers=headers, params=params)
-            response.raise_for_status()
-            vacancies = response.json()['objects']
-            vacancies_found += len(vacancies)
-            salaries = []
-            for vacancy in vacancies:
-                salary = predict_rub_salary_superjob(vacancy)
-                if salary:
-                    salaries.append(salary)
-                    vacancies_processed += 1
-            total_salaries.append(salaries)
-            time.sleep(1)
-        vacancies_by_language[language]['average_salary'] = int(np.mean([x for y in total_salaries for x in y]))
-        vacancies_by_language[language]['vacancies_found'] = vacancies_found
-        vacancies_by_language[language]['vacancies_processed'] = vacancies_processed
-        time.sleep(1)
-    return vacancies_by_language
+    auth_header = {"X-Api-App-Id": sj_secret_key}
+
+    statistics = get_vacancies_statistics(SJ_API_METHOD_URL, SJ_MAX_VACANCIES_QUANTITY, SJ_VACANCIES_PER_PAGE,
+                                          text_field_name='keyword',
+                                          location_field_name='town',
+                                          items_field_name='objects',
+                                          count_per_page_field_name='count',
+                                          salary_prediction_function=predict_rub_salary_superjob,
+                                          city_id=SJ_MOSCOW_ID,
+                                          period=period,
+                                          request_headers=auth_header,)
+    return statistics
 
 
 def main():
--- main.py
+++ main.py
@@ -1,38 +1,149 @@
+import os
+from dotenv import load_dotenv
 import requests
+import time
+from pprint import pprint
+from tqdm import tqdm
+import numpy as np
+
+POPULAR_LANGUAGES = [
+    'JavaScript',
+    'Java',
+    'Python',
+    'C#',
+    'TypeScript',
+    'PHP',
+    'Kotlin',
+    'C++'
+]
+
+HH_API_METHOD_URL = "https://api.hh.ru/vacancies"
+HH_MAX_VACANCIES_QUANTITY = 2000
+HH_VACANCIES_PER_PAGE = 100
+HH_MOSCOW_ID = 1
+
+SJ_API_METHOD_URL = "https://api.superjob.ru/2.0/vacancies/"
+SJ_MAX_VACANCIES_QUANTITY = 500
+SJ_VACANCIES_PER_PAGE = 100
+SJ_MOSCOW_ID = 4
 
 
-def predict_rub_salary_for_superjob(vacancy: dict):
-    if vacancy['currency'] == 'rub':
-        if vacancy['payment_from'] and vacancy['payment_to']:
-            salary = 0.5 * (vacancy['payment_from'] + vacancy['payment_to'])
-        elif vacancy['payment_from']:
-            salary = 1.2 * vacancy['payment_from']
-        elif vacancy['payment_to']:
-            salary = 0.8 * vacancy['payment_to']
-        else:
-            return None
-        return salary
+def predict_salary(salary_from, salary_to,
+                   salary_from_coef: float = 1.2,
+                   salary_to_coef: float = 0.8):
+    if salary_from and salary_to:
+        salary = (salary_from + salary_to) / 2  # mean
+    elif salary_from:
+        salary = salary_from_coef * salary_from
+    elif salary_to:
+        salary = salary_to_coef * salary_to
+    else:
+        return None
+    return salary
+
+
+def predict_rub_salary_hh(vacancy: dict):
+    salary = vacancy['salary']
+    if salary['currency'] == 'RUR':
+        return predict_salary(salary['from'],
+                              salary['to'])
     return None
 
 
-token = "v3.r.137457653.7ea316e15b259c72cbb425cd3ad5a3eef3eb21a2.fbe8484fa854ad9fd2cc55f3776bd60959941488"
-
-header = {"X-Api-App-Id": token}
-url = "https://api.superjob.ru/2.0/vacancies/"
-
-params = {
-    'keyword': 'программист C++',
-    'town': 4,
-    'period': 30,
-    'page':1 ,
-    'count': 100
-}
-
-response = requests.get(url, headers=header, params=params)
-for obj in response.json()['objects']:
-    print(obj['profession'], obj['town']['title'], sep=', ')
+def predict_rub_salary_superjob(vacancy: dict):
+    if vacancy['currency'] == 'rub':
+        return predict_salary(vacancy['payment_from'],
+                              vacancy['payment_to'])
+    return None
 
 
+def get_vacancies_statistics(
+        api_method_url: str,
+        max_vacancies_quantity: int,
+        vacancies_per_page: int,
+        text_field_name: str,
+        location_field_name: str,
+        items_field_name: str,
+        city_id: int,
+        salary_prediction_function: callable,
+        period_field_name: str = 'period',
+        page_number_field_name: str = 'page',
+        count_per_page_field_name: str = 'per_page',
+        request_headers: dict = None,
+        additional_request_params: dict = None,
+        requests_delay: int = 5,
+        period: int = 30
+        ) -> dict:
+    vacancies_by_language = {key: {} for key in POPULAR_LANGUAGES}
+    for language in tqdm(POPULAR_LANGUAGES):
+        total_salaries = []
+        vacancies_found = 0
+        vacancies_processed = 0
+        for page_number in range(int(max_vacancies_quantity / vacancies_per_page)):
+            headers = request_headers
+            params = {
+                text_field_name: f'Программист {language}',
+                location_field_name: city_id,
+                period_field_name: period,
+                page_number_field_name: page_number,
+                count_per_page_field_name: vacancies_per_page,
+            }
+            if additional_request_params:
+                params += additional_request_params
+            response = requests.get(api_method_url, headers=headers, params=params)
+            response.raise_for_status()
+            vacancies = response.json()[items_field_name]
+            vacancies_found += len(vacancies)
+            salaries = []
+            for vacancy in vacancies:
+                salary = salary_prediction_function(vacancy)
+                if salary:
+                    salaries.append(salary)
+                    vacancies_processed += 1
+            total_salaries.append(salaries)
+            time.sleep(1)
+        vacancies_by_language[language]['average_salary'] = int(np.mean([x for y in total_salaries for x in y]))
+        vacancies_by_language[language]['vacancies_found'] = vacancies_found
+        vacancies_by_language[language]['vacancies_processed'] = vacancies_processed
+        time.sleep(requests_delay)
+    return vacancies_by_language
 
 
+def get_vacancies_statistics_hh(period: int = 30):
+    additional_request_param = {'only_with_salary': True}
 
+    statistics = get_vacancies_statistics(HH_API_METHOD_URL, HH_MAX_VACANCIES_QUANTITY, HH_VACANCIES_PER_PAGE,
+                                          text_field_name='text',
+                                          location_field_name='area',
+                                          items_field_name='items',
+                                          salary_prediction_function=predict_rub_salary_hh,
+                                          city_id=HH_MOSCOW_ID,
+                                          period=period,
+                                          additional_request_params=additional_request_param,
+                                          requests_delay=10)
+    return statistics
+
+
+def get_vacancies_statistics_sj(sj_secret_key: str, period: int = 30):
+    auth_header = {"X-Api-App-Id": sj_secret_key}
+
+    statistics = get_vacancies_statistics(SJ_API_METHOD_URL, SJ_MAX_VACANCIES_QUANTITY, SJ_VACANCIES_PER_PAGE,
+                                          text_field_name='keyword',
+                                          location_field_name='town',
+                                          items_field_name='objects',
+                                          count_per_page_field_name='count',
+                                          salary_prediction_function=predict_rub_salary_superjob,
+                                          city_id=SJ_MOSCOW_ID,
+                                          period=period,
+                                          request_headers=auth_header,)
+    return statistics
+
+
+def main():
+    load_dotenv()
+    sj_secret_key = os.getenv('SJ_SECRET_KEY')
+    pprint(get_vacancies_statistics_sj(sj_secret_key))
+
+
+if __name__ == "__main__":
+    main()
