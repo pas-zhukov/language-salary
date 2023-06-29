faa89e0eb3e5caa9a1068df94257767f52a5c9d4
Pavel <pas-zhukov@yandex.ru>
2023-06-29 21:06:51 +0300

    add docstring

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
-import random
+import time
 
 from dotenv import load_dotenv
-import requests
-import time
-from pprint import pprint
-from tqdm import tqdm
 import numpy as np
+import requests
+from tqdm import tqdm
 from terminaltables import SingleTable
 
 POPULAR_LANGUAGES = [
@@ -41,11 +39,18 @@ def main():
 
 
 def print_statistics_table(site_city_name: str, statistics: dict):
+    """
+    Prints table with vacancies statistics.
+
+    Prints a table with statistics of vacancies found,
+    processed, and average salary for each programming
+    language in a given city and job search site.
+    :param site_city_name: name of the job search site and city
+    :param statistics: statistics of vacancies found
+    :return: None
+    """
     headers = ['Programming Language', 'Vacancies Found', 'Vacancies Processed', 'Average Salary']
-    rows = []
-    for language in statistics.keys():
-        row = [language] + list(statistics[language].values())
-        rows.append(row)
+    rows = [[language] + list(language_stats.values()) for language, language_stats in statistics.items()]
     table_title = site_city_name
     table_instance = SingleTable([headers] + rows, table_title)
     table_instance.justify_columns[1] = 'center'
@@ -57,6 +62,15 @@ def print_statistics_table(site_city_name: str, statistics: dict):
 def predict_salary(salary_from, salary_to,
                    salary_from_coef: float = 1.2,
                    salary_to_coef: float = 0.8):
+    """
+    Predicts the average salary based on the given salary from/to values
+
+    :param salary_from: lower salary bound
+    :param salary_to: upper salary bound
+    :param salary_from_coef: adjusting coefficient if 'salary_from' is the only available value
+    :param salary_to_coef: adjusting coefficient if 'salary_to' is the only available value
+    :return: predicted salary
+    """
     if salary_from and salary_to:
         salary = (salary_from + salary_to) / 2  # mean
     elif salary_from:
@@ -69,6 +83,12 @@ def predict_salary(salary_from, salary_to,
 
 
 def predict_rub_salary_hh(vacancy: dict):
+    """
+    Predicts salary based on the vacancy from HeadHunter site.
+
+    :param vacancy: a dictionary containing information about a job vacancy
+    :return: predicted salary
+    """
     salary = vacancy['salary']
     if salary['currency'] == 'RUR':
         return predict_salary(salary['from'],
@@ -77,6 +97,12 @@ def predict_rub_salary_hh(vacancy: dict):
 
 
 def predict_rub_salary_superjob(vacancy: dict):
+    """
+    Predicts salary based on the vacancy from SuperJob site.
+
+    :param vacancy: a dictionary containing information about a job vacancy
+    :return: predicted salary
+    """
     if vacancy['currency'] == 'rub':
         return predict_salary(vacancy['payment_from'],
                               vacancy['payment_to'])
@@ -100,6 +126,29 @@ def get_vacancies_statistics(
         requests_delay: int = 5,
         period: int = 30
         ) -> dict:
+    """
+    Collects and computes vacancies statistics.
+
+    The objective of the function is to retrieve statistics
+    of vacancies found, processed, and average salary for
+    each programming language in a given city and job search site
+    :param api_method_url: URL of the API method to retrieve vacancies data
+    :param max_vacancies_quantity: maximum number of vacancies to retrieve, based on API limitations
+    :param vacancies_per_page: number of vacancies to retrieve per page, based on API limitations
+    :param text_field_name: [API setting] name of the field to search for a specific keyword
+    :param location_field_name: [API setting] name of the field to search for a specific location
+    :param items_field_name: [API setting] name of the field containing the vacancies data
+    :param city_id: ID of the city to search for vacancies
+    :param salary_prediction_function: function to predict the salary of a vacancy, based on API you use
+    :param period_field_name: [API setting] name of the field to search for vacancies posted within a specific period
+    :param page_number_field_name: [API setting] name of the field to specify the page number of the vacancies data
+    :param count_per_page_field_name: [API setting] name of the field to specify the number of vacancies per page
+    :param request_headers: headers to include in the API request
+    :param additional_request_params:  additional parameters to include in the API request
+    :param requests_delay: delay in seconds between API requests
+    :param period: period in days to search for vacancies posted within
+    :return: statistics of vacancies found, processed, and average salary for each programming language
+    """
     vacancies_by_language = {key: {} for key in POPULAR_LANGUAGES}
     for language in tqdm(POPULAR_LANGUAGES):
         total_salaries = []
@@ -136,6 +185,15 @@ def get_vacancies_statistics(
 
 
 def get_vacancies_statistics_hh(period: int = 30):
+    """
+    Collects vacancies statistics from HeadHunter.
+
+    Collects and computes statistics of vacancies found,
+    processed, and average salary for each programming language
+    in Moscow using the HeadHunter job search site.
+    :param period: period in days to search for vacancies posted within
+    :return: statistics of vacancies found, processed, and average salary for each programming language
+    """
     additional_request_param = {'only_with_salary': True}
 
     statistics = get_vacancies_statistics(HH_API_METHOD_URL, HH_MAX_VACANCIES_QUANTITY, HH_VACANCIES_PER_PAGE,
@@ -151,6 +209,16 @@ def get_vacancies_statistics_hh(period: int = 30):
 
 
 def get_vacancies_statistics_sj(sj_secret_key: str, period: int = 30):
+    """
+    Collects vacancies statistics from SuperJob.
+
+    Collects and computes statistics of vacancies found,
+    processed, and average salary for each programming language
+    in Moscow using the SuperJob job search site.
+    :param sj_secret_key: secret key from your SJ API application page
+    :param period: period in days to search for vacancies posted within
+    :return: statistics of vacancies found, processed, and average salary for each programming language
+    """
     auth_header = {"X-Api-App-Id": sj_secret_key}
 
     statistics = get_vacancies_statistics(SJ_API_METHOD_URL, SJ_MAX_VACANCIES_QUANTITY, SJ_VACANCIES_PER_PAGE,
